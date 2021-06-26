21e874c8cad975b91503cc4e54afe3ea4ce8189f
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2021-06-26 15:34:02 -0400

    Update activity.php

--- activity.php
+++ activity.php
@@ -4,6 +4,7 @@ require_once("inc/header.php");
 ?>
 <body>
     <div class="no-content">
+            <?php require_once("inc/itrllyisnt.php"); ?>
             <p>Loading Activity Feed...</p> <!-- classic trick to say "Activity feed is loading, but it's really not." -->
     </div>
 </body>
